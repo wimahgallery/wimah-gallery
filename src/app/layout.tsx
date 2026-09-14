146769e8ef87a56a739260b8d572acc9527447a0
Fandoy <fandyglitch3@gmail.com>
2026-09-14 08:41:49 +0800

    feat: google verificatiion

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -50,6 +50,9 @@ export const metadata: Metadata = {
     type: "website",
     locale: "id_ID",
   },
+  verification: {
+    google: "HxdIo1z15LOPRlwKfQYz8LKqejKMerolkHi_Ugqfvcs",
+  },
 };
 
 export default function RootLayout({ children }: PropsWithChildren) {
